8b4ce76afe2df6299ae8e8bafd2aa37fc091ad07
Adel Koljenovic <adel.koljenovic@outlook.com>
2018-07-10 14:29:51 -0400

    create products for the db in the seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.new(name: "Harry's Blades", price: 16, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/f5162bc0587e45be9702a0518457aa79df38c643.jpg", description: "
+  5 German blades.")
+Product.new(name: "Shave Gel", price: 6, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/b2ee7312be64beaaaa8b003499fe82afe47ef3a5.jpg", description: "The rich lather protects your face and keeps you in control of your shave.")
+Product.new(name: "The Truman", price: 9, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/3c2b79bd2a0ef3025b1df8d478f9de679067092e.jpg", description: "A rubberized handle with a textured pattern provides optimal control, even when wet.")
+Product.new(name: "Post-Shave Balm", price: 8, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/33ef2a0ac7e39c9c82d00e618fb25f7d1f3c3560.jpg", description: "Designed to calm, balance, and soothe your skin after you shave.")

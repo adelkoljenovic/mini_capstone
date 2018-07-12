3461f400b5dcf0c782af98f03912edf4ec4755da
Adel Koljenovic <adel.koljenovic@outlook.com>
2018-07-11 21:35:04 -0400

    assign supplier_ids to all products

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,18 @@
 # Product.create(name: "The Truman", price: 9, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/3c2b79bd2a0ef3025b1df8d478f9de679067092e.jpg", description: "A rubberized handle with a textured pattern provides optimal control, even when wet.")
 # Product.create(name: "Post-Shave Balm", price: 8, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/33ef2a0ac7e39c9c82d00e618fb25f7d1f3c3560.jpg", description: "Designed to calm, balance, and soothe your skin after you shave.")
 
-Supplier.create(name: "Harry's German Industries", email: "markel@germany.com", phone_number: "123-555-5678")
-Supplier.create(name: "Harry's American Industries", email: "donald@usa.com", phone_number: "123-555-5679")
-Supplier.create(name: "Harry's Canadian Industries", email: "trudaeu@canada.com", phone_number: "123-555-5670")
+# Supplier.create(name: "Harry's German Industries", email: "markel@germany.com", phone_number: "123-555-5678")
+# Supplier.create(name: "Harry's American Industries", email: "donald@usa.com", phone_number: "123-555-5679")
+# Supplier.create(name: "Harry's Canadian Industries", email: "trudaeu@canada.com", phone_number: "123-555-5670")
+
+# associate the products with a supplier
+
+suppliers = Supplier.all
+
+products = Product.all
+
+products.each do |product|
+  product.supplier_id = suppliers.sample.id
+  product.save
+end
+

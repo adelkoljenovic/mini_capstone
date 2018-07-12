3e9e0a102cb50e95318daf9919541def99287b04
Adel Koljenovic <adel.koljenovic@outlook.com>
2018-07-11 21:04:54 -0400

    create new table for suppliers and seeded new table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Product.create(name: "Harry's Blades", price: 16, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/f5162bc0587e45be9702a0518457aa79df38c643.jpg", description: "
-  5 German blades.")
-Product.create(name: "Shave Gel", price: 6, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/b2ee7312be64beaaaa8b003499fe82afe47ef3a5.jpg", description: "The rich lather protects your face and keeps you in control of your shave.")
-Product.create(name: "The Truman", price: 9, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/3c2b79bd2a0ef3025b1df8d478f9de679067092e.jpg", description: "A rubberized handle with a textured pattern provides optimal control, even when wet.")
-Product.create(name: "Post-Shave Balm", price: 8, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/33ef2a0ac7e39c9c82d00e618fb25f7d1f3c3560.jpg", description: "Designed to calm, balance, and soothe your skin after you shave.")
+# Product.create(name: "Harry's Blades", price: 16, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/f5162bc0587e45be9702a0518457aa79df38c643.jpg", description: "
+#  5 German blades.")
+# Product.create(name: "Shave Gel", price: 6, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/b2ee7312be64beaaaa8b003499fe82afe47ef3a5.jpg", description: "The rich lather protects your face and keeps you in control of your shave.")
+# Product.create(name: "The Truman", price: 9, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/3c2b79bd2a0ef3025b1df8d478f9de679067092e.jpg", description: "A rubberized handle with a textured pattern provides optimal control, even when wet.")
+# Product.create(name: "Post-Shave Balm", price: 8, image_url: "https://www.harrys.com/harrys-cdnx-prod/assets/images/gallery_images/attachments/33ef2a0ac7e39c9c82d00e618fb25f7d1f3c3560.jpg", description: "Designed to calm, balance, and soothe your skin after you shave.")
+
+Supplier.create(name: "Harry's German Industries", email: "markel@germany.com", phone_number: "123-555-5678")
+Supplier.create(name: "Harry's American Industries", email: "donald@usa.com", phone_number: "123-555-5679")
+Supplier.create(name: "Harry's Canadian Industries", email: "trudaeu@canada.com", phone_number: "123-555-5670")
